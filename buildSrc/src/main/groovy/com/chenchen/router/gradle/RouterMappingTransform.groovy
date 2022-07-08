9c3e7274242ba706ae18bd3434e18b23cebaeae4
“chenqun” <1715666965@qq.com>
2022-07-08 17:39:57 +0800

    transform

---
+++ buildSrc/src/main/groovy/com/chenchen/router/gradle/RouterMappingTransform.groovy
@@ -0,0 +1,89 @@
+package com.chenchen.router.gradle
+
+import com.android.build.api.transform.QualifiedContent
+import com.android.build.api.transform.Transform
+import com.android.build.api.transform.TransformException
+import com.android.build.api.transform.TransformInvocation
+import com.android.build.gradle.internal.pipeline.TransformManager
+import com.android.utils.FileUtils
+
+class RouterMappingTransform extends Transform {
+
+    /**
+     * 当前 Transform 的名称
+     * @return
+     */
+    @Override
+    String getName() {
+        return "RouterMappingTransform"
+    }
+
+    /**
+     * 返回告知编译器，当前Transform需要消费的输入类型
+     * 在这里是CLASS类型
+     * @return
+     */
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    /**
+     * 告知编译器，当前Transform需要收集的范围
+     * @return
+     */
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    /**
+     * 是否支持增量
+     * 通常返回False
+     * @return
+     */
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+
+    /**
+     * 所有的class收集好以后，会被打包传入此方法
+     * @param transformInvocation
+     * @throws TransformException
+     * @throws InterruptedException
+     * @throws IOException
+     */
+    @Override
+    void transform(TransformInvocation transformInvocation)
+            throws TransformException, InterruptedException, IOException {
+        // 1. 遍历所有的Input
+        // 2. 对Input进行二次处理
+        // 3. 将Input拷贝到目标目录(此步骤是必须的，否则返回的字节码是空的)
+
+        // 遍历所有的输入
+        transformInvocation.inputs.each {
+
+            // 把 文件夹 类型的输入，拷贝到目标目录
+            it.directoryInputs.each { directoryInput ->
+                def destDir = transformInvocation.outputProvider
+                        .getContentLocation(
+                                directoryInput.name,
+                                directoryInput.contentTypes,
+                                directoryInput.scopes,
+                                Format.DIRECTORY)
+                FileUtils.copyDirectory(directoryInput.file, destDir)
+            }
+
+            // 把 JAR 类型的输入，拷贝到目标目录
+            it.jarInputs.each { jarInput ->
+                def dest = transformInvocation.outputProvider
+                        .getContentLocation(
+                                jarInput.name,
+                                jarInput.contentTypes,
+                                jarInput.scopes, Format.JAR)
+                FileUtils.copyFile(jarInput.file, dest)
+            }
+        }
+    }
+}
--- buildSrc/src/main/groovy/com/chenchen/router/gradle/RouterPlugin.groovy
+++ buildSrc/src/main/groovy/com/chenchen/router/gradle/RouterPlugin.groovy
@@ -1,5 +1,8 @@
 package com.chenchen.router.gradle
 
+import com.android.build.api.transform.Transform
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.AppPlugin
 import groovy.json.JsonSlurper
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -8,6 +11,14 @@ class RouterPlugin implements Plugin<Project>{
     //实现apply方法，注入插件的逻辑，被应用是就会调用
     @Override
     void apply(Project project) {
+        //注册 Transform
+        //有AppPlugin 代表是com.android.application 的工程，才能注册
+        if (project.plugins.hasPlugin(AppPlugin)) {
+            AppExtension appExtension = project.extensions.getByType(AppExtension)
+            Transform transform = new RouterMappingTransform()
+            appExtension.registerTransform(transform)
+        }
+
         // 1. 自动帮助用户传递路径参数到注解处理器中
 //        kapt {
 //            arguments {
@@ -28,6 +39,7 @@ class RouterPlugin implements Plugin<Project>{
                 routerMappingDir.deleteDir()
             }
         }
+        if(!project.plugins.hasPlugin(AppPlugin)) return
 
         println("我是 RouterPlugin,apply from ${project.name}")
         //"router" 可以随便起
